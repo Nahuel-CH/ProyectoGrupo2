2748ecc06cad90f842819a13dcb7a5c20eae0cbf
Nahuel-CH <nchinchilla00023@ufide.ac.cr>
2025-11-11 17:31:37 -0600

    Update DBConnection.java

--- src/com/mycompany/proyectogrupo2/util/DBConnection.java
+++ src/com/mycompany/proyectogrupo2/util/DBConnection.java
@@ -7,9 +7,10 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL  = "jdbc:mysql://localhost:3306/inventario_db?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASS = "0414"; //
+    private static final String PASS = "root"; //Esta parte de acá puede modificarse, dependiendo de las credenciales de la base de datos a la que se conecte.
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
+
